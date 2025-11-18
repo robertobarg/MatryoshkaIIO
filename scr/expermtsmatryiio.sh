e04a16723f7ac8ccfbf41122d5f04d84a163f4f3
roberto.bargetto <roberto.bargetto@gmail.com>
2025-11-18 15:49:25 +0100

    Script scr/expermtsmatryiio.sh reviewed

--- scr/expermtsmatryiio.sh
+++ scr/expermtsmatryiio.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-    echo "Error: This script requires exactly 3 arguments" >&2
-    echo "Usage: $0 <arg1> <arg2> <arg3>" >&2
+if [ $# -ne 4 ]; then
+    echo "Error: This script requires exactly 4 arguments" >&2
+    echo "Usage: $0 <arg1> <arg2> <arg3> <arg4>" >&2
     exit 1
 fi
 
 # executable file
-EXE=${1}
+EXE=$(readlink -e ${1})
 IDIR=$(readlink -e ${2})
+CFG=$(readlink -e ${3})
 # number of precesses
-PRS=${3}
+if [[ "${4}" =~ ^[0-9]+$ ]]; then
+    PRS="${4}"
+else
+    PRS=1
+fi
+
+echo "$PRS parallel experiments"
+
+# tests
+if [ $? -ne 0 ] || [ -z "$EXE" ]; then
+    echo "Error: File '$1' does not exist or is broken link" >&2
+    exit 1
+fi
+if [ $? -ne 0 ] || [ -z "$IDIR" ]; then
+    echo "Error: File '$2' does not exist or is broken link" >&2
+    exit 1
+fi
+if [ $? -ne 0 ] || [ -z "$CFG" ]; then
+    echo "Error: File '$3' does not exist or is broken link" >&2
+    exit 1
+fi
+
 
 # test that data dir is a subdir
 if [[ $(realpath --relative-base="$PWD" "$IDIR") == /* ]]; then
@@ -21,47 +43,81 @@ fi
 # Continue with your script here
 echo "Safe to proceed"
 
+CFGSTR=$(cat $CFG | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
 # prepare file of direcitives
-echo "${IDIR}/MicroscopyImages;_ALG_ 18111 5 46 518400 0;_OF_" > dirfile.csv
-echo "${IDIR}/Shapes;_ALG_ 18111 5 46 518400 0;_OF_" >> dirfile.csv
-echo "${IDIR}/GRFrough;_ALG_ 18111 5 46 518400 0;_OF_" >> dirfile.csv
-echo "${IDIR}/ClassicImages;_ALG_ 18111 5 46 518400 0;_OF_" >> dirfile.csv
-echo "${IDIR}/GRFmoderate;_ALG_ 18111 5 46 518400 0;_OF_" >> dirfile.csv
-echo "${IDIR}/GRFsmooth;_ALG_ 18111 5 46 518400 0;_OF_" >> dirfile.csv
-echo "${IDIR}/LogitGRF;_ALG_ 18111 5 46 518400 0;_OF_" >> dirfile.csv
-echo "${IDIR}/CauchyDensity;_ALG_ 18111 5 46 518400 0;_OF_" >> dirfile.csv
-echo "${IDIR}/LogGRF;_ALG_ 18111 5 46 518400 0;_OF_" >> dirfile.csv
-echo "${IDIR}/WhiteNoise;_ALG_ 18111 5 46 518400 0;_OF_" >> dirfile.csv
+echo "${IDIR}/MicroscopyImages;$CFGSTR;_OF_" > dirfile.csv
+echo "${IDIR}/Shapes;$CFGSTR;_OF_" >> dirfile.csv
+echo "${IDIR}/GRFrough;$CFGSTR;_OF_" >> dirfile.csv
+echo "${IDIR}/ClassicImages;$CFGSTR;_OF_" >> dirfile.csv
+echo "${IDIR}/GRFmoderate;$CFGSTR;_OF_" >> dirfile.csv
+echo "${IDIR}/GRFsmooth;$CFGSTR;_OF_" >> dirfile.csv
+echo "${IDIR}/LogitGRF;$CFGSTR;_OF_" >> dirfile.csv
+echo "${IDIR}/CauchyDensity;$CFGSTR;_OF_" >> dirfile.csv
+echo "${IDIR}/LogGRF;$CFGSTR;_OF_" >> dirfile.csv
+echo "${IDIR}/WhiteNoise;$CFGSTR;_OF_" >> dirfile.csv
 
 # clear instance list files
 [[ -n "$IDIR" ]] && [[ -d "$IDIR" ]] && find "${IDIR}/" -type f -regex ".*/list[0-9]+\.txt$" -exec rm -v {} \;
 
 # generate file for list of sizes
 echo  "32"  > listsizes.txt 
-echo  "64" >> listsizes.txt 
-echo "128" >> listsizes.txt 
-echo "256" >> listsizes.txt 
+#echo  "64" >> listsizes.txt 
+#echo "128" >> listsizes.txt 
+#echo "256" >> listsizes.txt 
 
 # generate instance list file
 ./scr/gendotmarklists.sh ${IDIR} listsizes.txt
 
+#create dir
+DDIR=$(mktemp -d -p $(pwd) matryoskhaiio4d.XXXXXXXX)
+if [ $? -ne 0 ] || [ ! -d "$DDIR" ]; then
+    echo "Error: Failed to create temporary directory" >&2
+    exit 1
+fi
+
 # run matryoshka experiments: OF 1
-DDIR=$(mktemp -d -p $(pwd) kukly.of1.XXXXXXXX) && \
-    sed -e "s/_ALG_/rd/" -e "s/_OF_/1/" dirfile.csv > matrydirfile.csv && \
+sed -e "s/_OF_/1/" dirfile.csv > matrydirfile.csv && \
     ./scr/multidir.sh scr/multicfg.sh scr/batchpl.sh $EXE matrydirfile.csv $DDIR $PRS
 
 # run matryoshka experiments: OF 2
-DDIR=$(mktemp -d -p $(pwd) kukly.of2.XXXXXXXX) && \
-    sed -e "s/_ALG_/rd/" -e "s/_OF_/2/" dirfile.csv > matrydirfile.csv && \
+sed -e "s/_OF_/2/" dirfile.csv > matrydirfile.csv && \
     ./scr/multidir.sh scr/multicfg.sh scr/batchpl.sh $EXE matrydirfile.csv $DDIR $PRS
 
 # run matryoshka experiments: OF 3
-DDIR=$(mktemp -d -p $(pwd) kukly.of3.XXXXXXXX) && \
-    sed -e "s/_ALG_/rd/" -e "s/_OF_/3/" dirfile.csv > matrydirfile.csv && \
+sed -e "s/_OF_/3/" dirfile.csv > matrydirfile.csv && \
     ./scr/multidir.sh scr/multicfg.sh scr/batchpl.sh $EXE matrydirfile.csv $DDIR $PRS
 
 # run matryoshka experiments: OF 4
-DDIR=$(mktemp -d -p $(pwd) kukly.of4.XXXXXXXX) && \
-    sed -e "s/_ALG_/rd/" -e "s/_OF_/4/" dirfile.csv > matrydirfile.csv && \
+sed -e "s/_OF_/4/" dirfile.csv > matrydirfile.csv && \
     ./scr/multidir.sh scr/multicfg.sh scr/batchpl.sh $EXE matrydirfile.csv $DDIR $PRS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
