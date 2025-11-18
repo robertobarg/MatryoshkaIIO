b7a94bf5d434d72285db4b1cc2d96888adebe479
roberto.bargetto <roberto.bargetto@gmail.com>
2025-11-18 15:55:51 +0100

    Script scr/geniptp.sh updated

--- scr/geniptp.sh
+++ scr/geniptp.sh
@@ -6,7 +6,11 @@ OBJF=${3}
 FN1=$(readlink -e ${4})
 FN2=$(readlink -e ${5})
 
-instfile=${CLS}_1_${SZ}_1_${SZ}_${SZ}_${SZ}_$(basename ${FN1} .csv | sed "s/data${SZ}_//")$(basename ${FN2} .csv | sed "s/data${SZ}_//").txt
+instfile=OF${OBJF}_${CLS}_1_${SZ}_1_${SZ}_${SZ}_${SZ}_$(basename ${FN1} .csv | sed "s/data${SZ}_//")$(basename ${FN2} .csv | sed "s/data${SZ}_//").txt
 
-printf "Gen instance ${instfile} ... "
-echo "$(($SZ * $SZ)) $(($SZ * $SZ)) $OBJF" > $instfile && sed -e ':a;N;$!ba;s/\n/,/g' -e "s/,/ /g" $FN1 >> $instfile && sed -e ':a;N;$!ba;s/\n/,/g' -e "s/,/ /g" $FN2 >> $instfile && echo "OK" || echo "KO"
+# Gen instance
+echo "$(($SZ * $SZ)) $(($SZ * $SZ)) $OBJF" > $instfile && \
+    sed -e ':a;N;$!ba;s/\n/,/g' -e "s/,/ /g" $FN1 >> $instfile && \
+    sed -e ':a;N;$!ba;s/\n/,/g' -e "s/,/ /g" $FN2 >> $instfile
+
+echo $instfile
