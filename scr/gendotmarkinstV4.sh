0bffb2238173b535a7e51d74f8ae82dd318adce7
roberto.bargetto <roberto.bargetto@gmail.com>
2025-11-18 13:59:43 +0100

    Script scr/gendotmarkinstV4.sh added

---
+++ scr/gendotmarkinstV4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CLS=${1}
+SZ=${2}
+OBJF=${3}
+FN1=$(readlink -e ${4})
+FN2=$(readlink -e ${5})
+DDIR=$(readlink -e ${6})
+
+instfile=OF${OBJF}_${CLS}_1_${SZ}_1_${SZ}_${SZ}_${SZ}_$(basename ${FN1} .csv | sed "s/data${SZ}_//")$(basename ${FN2} .csv | sed "s/data${SZ}_//").txt
+
+# Gen instance
+echo "$(($SZ * $SZ)) $(($SZ * $SZ)) $OBJF" > $DDIR/$instfile && \
+    sed -e ':a;N;$!ba;s/\n/,/g' -e "s/,/ /g" $FN1 >> $DDIR/$instfile && \
+    sed -e ':a;N;$!ba;s/\n/,/g' -e "s/,/ /g" $FN2 >> $DDIR/$instfile
+
+echo $DDIR/$instfile
