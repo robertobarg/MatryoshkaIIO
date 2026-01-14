230535128d3041849a24f915e08bc444469746ff
roberto.bargetto <roberto.bargetto@gmail.com>
2026-01-14 14:48:10 +0100

    Update scr/getresults.sh

--- scr/getresults.sh
+++ scr/getresults.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 DB=${1}
 SQL=$(readlink -e ${2})
 DIR=$(readlink -e ${3})
@@ -7,15 +6,19 @@ DIR=$(readlink -e ${3})
 TBNM=$(basename $DIR)
 RESF=$TBNM.csv.raw
 
+TBNM=$(echo "$TBNM" | tr '[:upper:]' '[:lower:]')
+TBNM=$(echo "$TBNM" | tr '.' '_')
+
 find "${DIR}/" -type f -name "*.optres" -exec cat {} \;  > $RESF
 sed -i -e "s/_/ /g" $RESF
 
-psql -c "drop table if exists \"${TBNM}\"" $DB
-psql -v tabname=\"${TBNM}\" -f ${SQL}/resultproc.sql $DB
-psql -c "copy \"${TBNM}\" from '$(readlink -e $RESF)' with null as 'nan' delimiter ' ' csv;" $DB
+psql -c "drop table if exists ${TBNM}" $DB
+psql -v tabname=${TBNM} -f ${SQL}/resultproc.sql $DB
 
-psql -v tab=\"${TBNM}\" -f ${SQL}/query.sql $DB > $TBNM.csv
+psql -d "$DB" -v ON_ERROR_STOP=1 <<EOF
+\copy "${TBNM}" FROM '$(readlink -e $RESF)' WITH NULL AS 'nan' DELIMITER ' ' CSV;
+EOF
+
+psql -v tab=${TBNM} -f ${SQL}/query.sql $DB > $TBNM.csv
 
 echo "Results in file $TBNM.csv"
-
-
